597fb4e8298ad6673aae9b109ddff9ab844b6b46
ShubhamP1520 <shubhampotadar3@gmail.com>
2025-08-21 22:42:49 +0530

    alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,15 @@
+package my_pkg;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+  `include "defines.sv"
+  `include "alu_seq_item.sv"
+  `include "alu_seq.sv"
+  `include "alu_driver.sv"
+  `include "alu_monitor.sv"
+  `include "alu_sequencer.sv"
+  `include "alu_agt.sv"
+  `include "alu_scb.sv"
+  `include "alu_cov.sv"
+  `include "alu_env.sv"
+  `include "alu_test.sv"
+endpackage
